98b546055c4d58f0e1f5d8e6b17909077bd42720
muktadiur <muktadiur@gmail.com>
2013-10-04 22:18:16 +0600

    initial commit

---
+++ SignalR.Client/SignalRClient.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+
+namespace SignalR.Client
+{
+    public class SignaRClient
+    {
+
+        private static string endPoint = ConfigurationManager.AppSettings.Get("SignalREndPoint");
+        private static string hubProxyName = ConfigurationManager.AppSettings.Get("HubProxyName");
+        private static HubConnection connection; 
+        private static IHubProxy hub;
+        private static readonly SignaRClient simpleClient = new SignaRClient();
+        public int ThreadSleepTime { get; set; }
+
+        private SignaRClient() {
+            connection = new HubConnection(endPoint);
+            hub = connection.CreateHubProxy(hubProxyName);
+            ThreadSleepTime = 500;
+        }
+        static SignaRClient() { }
+
+        public static SignaRClient Instance {
+            get { return simpleClient; }
+        }
+        
+        public void Join(string name)
+        {
+            //Remove(name);
+            connection.Start().ContinueWith(t => 
+            {
+                hub.Invoke("Join", name);
+            });
+        }
+
+        public void Join(string name, Action<string,string, string, string, string, string, string> callBack)
+        {
+            //Remove(name);
+            hub.On("Show", callBack);
+            connection.Start().ContinueWith(t =>
+            {
+                hub.Invoke("Join", name);
+            });
+        }
+
+        public void Invoke(params string[] messages)
+        {
+            Validate();
+            Thread.Sleep(ThreadSleepTime);
+            if (connection.State == ConnectionState.Connected)
+                hub.Invoke("Send", messages);
+            else {
+                connection.Start().ContinueWith(t =>
+                {
+                    hub.Invoke("Send", messages);
+                });
+            }
+        }
+        
+        public void Remove(string name)
+        {
+            if (connection.State == ConnectionState.Connected)
+                hub.Invoke("Remove", name).Wait();
+            else
+            {
+                connection.Start().ContinueWith(t =>
+                {
+                    hub.Invoke("Remove", name).Wait();
+                });
+            }
+            
+        }
+
+        private  void Validate()
+        {
+            if (string.IsNullOrEmpty(endPoint))
+                throw new ArgumentNullException("SignalREndPoint", "End Point URL not set in app config");
+            if (string.IsNullOrEmpty(hubProxyName))
+                throw new ArgumentNullException("HubProxyName", "Hub Proxy Name not set in app config");
+        }
+
+
+
+    }
+}
---
+++ SignalR.Sample/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SignalR.Client;
+
+namespace SignalR.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msg = string.Empty;
+            Console.Write("Please enter Name : ");
+            var name = Console.ReadLine();
+            SignaRClient client = SignaRClient.Instance;
+            Action<string, string, string, string, string, string, string> act = (a, b, c, d, e, f, g) => Show(a, b, c, d, e, f, g);
+            client.Join(name, act);
+            client.ThreadSleepTime = 10;
+            while (msg != "quit")
+            {
+                msg = Console.ReadLine();
+                new Thread(delegate()
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        client.Invoke(name, i.ToString(), "a", "b", "c", "d", "e");
+                    }
+                }).Start();
+
+
+                new Thread(delegate()
+                {
+                    for (int i = 100; i < 200; i++)
+                    {
+                        client.Invoke(name, i.ToString(), "a", "b", "c", "d", "e");
+                    }
+                }).Start();
+            }
+            
+        }
+        
+        public static string Show(string name, string msg, string a, string b, string c, string d,string e) {
+            var message = string.Format("{0} | {1} |  {2} | {3} | {4} | {5} | {6}", name, msg, a, b, c, d, e);
+            Console.WriteLine(message);
+            return message;
+        }
+    }
+}
